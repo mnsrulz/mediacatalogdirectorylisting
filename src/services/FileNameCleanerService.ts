ca053d9b85247027f564a508348dbedfcb454137
Manish Rawat <manishrawat4u@gmail.com>
2021-05-14 14:10:54 -0400

    bugfix parsing of filename

--- src/services/FileNameCleanerService.ts
+++ src/services/FileNameCleanerService.ts
@@ -1,6 +1,6 @@
 import { FileNode } from "../models/fileNode";
-import fileNameParser, { filenameParse } from '@ctrl/video-filename-parser';
-import logger from "./Logger"; 
+import { filenameParse, ParsedShow } from '@ctrl/video-filename-parser';
+import logger from "./Logger";
 
 import path from 'path';
 export class FileNameCleanerService {
@@ -24,16 +24,15 @@ export class FileNameCleanerService {
     }
 
     public CleanTvTitle(fileNodes: FileNode[], normalizedMediaName: string, year: string) {
-        function toTwoDigit(n: number): string {
-            return n > 9 ? "" + n : "0" + n;
-        }
+        const toTwoDigit = (n: number) => n > 9 ? `${n}` : `0${n}`;
+
         fileNodes.forEach(x => {
             let cleanedTitle = x.title;
             let extensionOfTitle = path.extname(x.title).toLowerCase();
 
             const knownExtensions = ['.mkv', '.mp4', '.m3u8'];
             if (knownExtensions.indexOf(extensionOfTitle) >= 0) {
-                const parsedFileName = filenameParse(x.title, true);
+                const parsedFileName = filenameParse(x.title, true) as ParsedShow;
                 // console.log(parsedFileName);
                 const seasonNumber = parsedFileName.seasons[0];
                 const episodeNumber = parsedFileName.episodeNumbers[0];
@@ -48,8 +47,8 @@ export class FileNameCleanerService {
             } else {
                 const parsedFileName = filenameParse(x.title, true);
                 // console.log(parsedFileName);
-                const seasonNumber = parsedFileName.seasons[0];
-                const episodeNumber = parsedFileName.episodeNumbers[0];
+                // const seasonNumber = parsedFileName.seasons[0];
+                // const episodeNumber = parsedFileName.episodeNumbers[0];
                 // console.log(`${seasonNumber}:${episodeNumber}`);
                 if (extensionOfTitle)
                     cleanedTitle = x.title.replace(/(\.[\w\d_-]+)$/i, `-${x.documentId}$1`);
